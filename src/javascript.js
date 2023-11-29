8f8360e1cf3c4631e26e3d6918e6e434eb2293ed
QuangTuan1109 <74809953+QuangTuan1109@users.noreply.github.com>
2023-11-29 09:44:00 +0700

    Initial server setup

---
+++ src/javascript.js
@@ -0,0 +1,49 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const morgan = require('morgan');
+
+// Middlewares
+app.use(morgan('dev'));
+
+// HTTP logger
+app.use(morgan("combined"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        message: 'Server is OK!'
+    })
+  })
+
+// Catch 404 Errors and forward them to error handler
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  });
+  
+  // Error handler function
+  app.use((err, req, res, next) => {
+    const error = app.get("env") === "development" ? err : {};
+    const status = err.status || 500;
+  
+    // response to client
+    return res.status(status).json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
